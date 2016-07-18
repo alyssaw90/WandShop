e54071e446a2463cc17b0d9af965e01432420e11
Alyssa Williams <alyssaw90@gmail.com>
2016-07-18 15:20:55 -0700

    added in c# files

---
+++ HarryPotterWandShop/HarryPotterWandShop/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotterWandShop
+{
+    class User
+    {
+        #region Properties
+        /// <summary>
+        /// User Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// User Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// User Email Address
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// User Password
+        /// </summary>
+        public string Password { get; set; }
+        #endregion
+    }
+}
---
+++ HarryPotterWandShop/HarryPotterWandShop/Wand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotterWandShop
+{
+    class Wand
+    {
+        #region Properties
+        /// <summary>
+        /// Wand Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Wand Name
+        /// </summary>
+        public string WandName { get; set; }
+
+        /// <summary>
+        /// Wand Description
+        /// </summary>
+        public string WandDescription { get; set; }
+
+        /// <summary>
+        /// Wand Price
+        /// </summary>
+        public decimal WandPrice { get; set; }
+        #endregion
+    }
+}
